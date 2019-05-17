786c8b94b6b95f883420540f1fc081f6365c8b1c
mingz <305603665@qq.com>
2019-05-17 11:35:31 +0800

    scripts

--- test/test_api_circle.sh
+++ test/test_api_circle.sh
@@ -3,10 +3,15 @@ i=0
 times=50
 while  (( $i <= $times ));
 do
-#    curl -XPOST -H 'Content-Type: application/json'  http://localhost:8080/todos/todo/add -d '{"title":"item123", "content":"asaaaaaaaa"}'
-    curl -XPOST -H 'Content-Type: application/json'  http://localhost:8080/todos/todo/list -d '{"title":"item123", "content":"asaaaaaaaa"}'
-    let i++
+(
+    #    curl -XPOST -H 'Content-Type: application/json'  http://localhost:8080/todos/todo/add -d '{"title":"item123", "content":"asaaaaaaaa"}'
+    curl -XPOST -H 'Content-Type: application/json'  http://localhost:8080/todos/todo/list -d '{}'
+
     echo '\n'
     echo $i
     echo '\n'
+)& # 小括号里面的会fork一个子进程执行
+let i++
+echo $!
 done;
+wait;
